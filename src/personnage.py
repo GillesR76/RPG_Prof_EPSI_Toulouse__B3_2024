a3ba94358ee2adfa63abdff2374ff742bb99de75
Enzo Sandre <enzo@enzosandre.fr>
2024-10-24 15:23:23 +0200

    [GREEN]

--- src/personnage.py
+++ src/personnage.py
@@ -1,5 +1,11 @@
 class Personnage:
+    def __init__(self):
+        self.__hp = 10
+
     def get_hp(self):
-        return 10
+        return self.__hp
+
+    def recevoir_attaque(self, attaquant):
+        self.__hp = 9
 
     
